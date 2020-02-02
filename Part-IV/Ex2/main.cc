639ab63ca7be226b32e9352a89fdd1760475ce67
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-01 22:15:05 -0500

    added files for Ex2

---
+++ Part-IV/Ex2/main.cc
@@ -0,0 +1,12 @@
+#include "print.h"
+
+int main()
+{
+    int a = 5;
+    float b = 5.5;
+
+    print(a);
+    print(a, b);
+
+    return 0;
+}
---
+++ Part-IV/Ex2/print.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <iostream>
+
+void print(int);
+void print(int, float);
+
+#endif
