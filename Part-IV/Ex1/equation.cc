759b65d8996177ae917c95a8111bf772411e7f8d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-01 22:01:51 -0500

    added ex1 for Part IV

---
+++ Part-IV/Ex1/equation.cc
@@ -0,0 +1,6 @@
+#include "equation.h"
+
+double equation(double x, double y, int a)
+{
+   return y * std::pow(x, 2.0) + a;
+}
---
+++ Part-IV/Ex1/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "equation.h"
+
+int main()
+{
+
+   std::cout << "equation (default) output = " << equation(2.0, 3.0) << std::endl;
+   std::cout << "equation (a = 10) output = " << equation(2.0, 3.0, 10.0) << std::endl;
+
+   return 0;
+}
+
