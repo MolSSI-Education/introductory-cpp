f21e831674e78be77200e78be4616b0654f62122
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-01 23:35:43 -0500

    del blank line

--- Part-IV/Ex1/main.cc
+++ Part-IV/Ex1/main.cc
@@ -3,7 +3,6 @@
 
 int main()
 {
-
    std::cout << "equation (default) output = " << equation(2.0, 3.0) << std::endl;
    std::cout << "equation (a = 10) output = " << equation(2.0, 3.0, 10.0) << std::endl;
 
