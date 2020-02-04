2929958b867b1c6b64177bdefc44e657d5de1f90
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-04 02:42:45 -0500

    updated Ex1

---
+++ Part-III/Ex1/main.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+
+// complete definition of factorial function
+unsigned long int factorial(unsigned long int n) {};
+
+int main()
+{
+    unsigned long int n;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
+    std::cout << "Factorial of " << n << " = " << factorial(n) << std::endl;
+    return 0;
+}
