e2609d72ebdac5d045191106014ff8d0e6a8394e
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-04 10:55:45 -0500

    updated print_int.cc

--- Part-II/Ex3/print_int.cc
+++ Part-II/Ex3/print_int.cc
@@ -1,9 +1,4 @@
 #include <iostream>
 #include "print_int.h"
 
-void ns1::print(int integer)
-{
-    std::cout << "integer from namespace ns1 = " << integer << std::endl;
-}
-
-// Define below a print function for namespace ns2
+// Define below 2 print functions for namespaces ns1 and ns2
