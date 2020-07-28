04b0d4f5a320e38dce5f37f7fd2009b1a22ddc37
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-07-28 15:50:16 -0400

    added Ex2

---
+++ Part-I/Ex2/library.cc
@@ -0,0 +1,6 @@
+#include "library.h"
+
+void print_lib()
+{
+    std::cout << "Library successfully linked." << std::endl;
+}
---
+++ Part-I/Ex2/main.cc
@@ -0,0 +1,10 @@
+#include "library.h"
+
+int main()
+{
+
+    std::cout << "Hello world!" << std::endl;
+    print_lib();
+
+    return 0;
+}
