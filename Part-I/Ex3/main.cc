f7b489f28e3d5127ca83354d02d8cd9a7ef24cbf
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-07-30 14:42:08 -0400

    added Ex3

---
+++ Part-I/Ex3/main.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int weight;
+    float height;
+
+    std::cout << "Enter your weight in Kgs: ";
+    std::cin >> weight;
+
+    std::cout << "Enter your height in meters: ";
+    std::cin >> height;
+
+    std::cout << "Your BMI (body mass index) is: " << weight / pow(height, 2) << std::endl;
+
+    return 0;
+}
